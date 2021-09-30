6ed83d3e01449004e87819fecf78d48c54e4ba89
TheAlan404 <43997085+TheAlan404@users.noreply.github.com>
2021-09-30 18:38:58 +0300

    better code + create ProtodefContext and ProtodefNamespace

--- ProtodefSharp/DefaultTypes.cs
+++ ProtodefSharp/DefaultTypes.cs
@@ -8,11 +8,11 @@ namespace ProtodefSharp.DefaultTypes
 	public class ProtoVoid : ProtodefType
 	{
 		public new string Id = "void";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			return JToken.FromObject(null);
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
 			return;
 		}
@@ -21,15 +21,15 @@ namespace ProtodefSharp.DefaultTypes
 	public class ProtoBool : ProtodefType
 	{
 		public new string Id = "bool";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
-			return JToken.FromObject(stream.ReadByte() == 1);
+			return JToken.FromObject(ctx.Stream.ReadByte() == 1);
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			bool bo = data.ToObject<bool>();
+			bool bo = ctx.Input.ToObject<bool>();
 			byte by = bo ? (byte) 0x01 : (byte) 0x00;
-			stream.WriteByte(by);
+			ctx.Stream.WriteByte(by);
 		}
 	}
 
@@ -38,168 +38,168 @@ namespace ProtodefSharp.DefaultTypes
 	public class ProtoByte : ProtodefType
 	{
 		public new string Id = "i8";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
-			return JToken.FromObject(stream.ReadByte()); // TODO: fix, -128 in read, +128 in write
+			return JToken.FromObject(ctx.Stream.ReadByte()); // TODO: fix, -128 in read, +128 in write
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			stream.WriteByte(data.ToObject<byte>());
+			ctx.Stream.WriteByte(ctx.Input.ToObject<byte>());
 		}
 	}
 
 	public class ProtoUByte : ProtodefType
 	{
 		public new string Id = "u8";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
-			return JToken.FromObject(stream.ReadByte());
+			return JToken.FromObject(ctx.Stream.ReadByte());
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			stream.WriteByte(data.ToObject<byte>());
+			ctx.Stream.WriteByte(ctx.Input.ToObject<byte>());
 		}
 	}
 
 	public class ProtoShort : ProtodefType
 	{
 		public new string Id = "i16";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[2];
-			stream.Read(data, 0, 2);
+			ctx.Stream.Read(data, 0, 2);
 			return JToken.FromObject(BitConverter.ToInt16(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<short>());
-			stream.Write(buf, 0, 2);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<short>());
+			ctx.Stream.Write(buf, 0, 2);
 		}
 	}
 
 	public class ProtoUShort : ProtodefType
 	{
 		public new string Id = "u16";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[2];
-			stream.Read(data, 0, 2);
+			ctx.Stream.Read(data, 0, 2);
 			return JToken.FromObject(BitConverter.ToUInt16(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<ushort>());
-			stream.Write(buf, 0, 2);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<ushort>());
+			ctx.Stream.Write(buf, 0, 2);
 		}
 	}
 
 	public class ProtoInt : ProtodefType
 	{
 		public new string Id = "i32";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[4];
-			stream.Read(data, 0, 4);
+			ctx.Stream.Read(data, 0, 4);
 			return JToken.FromObject(BitConverter.ToInt32(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<int>());
-			stream.Write(buf, 0, 4);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<int>());
+			ctx.Stream.Write(buf, 0, 4);
 		}
 	}
 
 	public class ProtoUInt : ProtodefType
 	{
 		public new string Id = "u32";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[4];
-			stream.Read(data, 0, 4);
+			ctx.Stream.Read(data, 0, 4);
 			return JToken.FromObject(BitConverter.ToUInt32(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<uint>());
-			stream.Write(buf, 0, 4);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<uint>());
+			ctx.Stream.Write(buf, 0, 4);
 		}
 	}
 
 	public class ProtoFloat : ProtodefType
 	{
 		public new string Id = "f32";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[4];
-			stream.Read(data, 0, 4);
+			ctx.Stream.Read(data, 0, 4);
 			return JToken.FromObject(BitConverter.ToSingle(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<float>());
-			stream.Write(buf, 0, 4);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<float>());
+			ctx.Stream.Write(buf, 0, 4);
 		}
 	}
 
 	public class ProtoDouble : ProtodefType
 	{
 		public new string Id = "f64";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[8];
-			stream.Read(data, 0, 8);
+			ctx.Stream.Read(data, 0, 8);
 			return JToken.FromObject(BitConverter.ToDouble(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<double>());
-			stream.Write(buf, 0, 8);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<double>());
+			ctx.Stream.Write(buf, 0, 8);
 		}
 	}
 
 	public class ProtoLong : ProtodefType
 	{
 		public new string Id = "i64";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[8];
-			stream.Read(data, 0, 8);
+			ctx.Stream.Read(data, 0, 8);
 			return JToken.FromObject(BitConverter.ToInt64(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<long>());
-			stream.Write(buf, 0, 8);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<long>());
+			ctx.Stream.Write(buf, 0, 8);
 		}
 	}
 
 	public class ProtoULong : ProtodefType
 	{
 		public new string Id = "u64";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			byte[] data = new byte[8];
-			stream.Read(data, 0, 8);
+			ctx.Stream.Read(data, 0, 8);
 			return JToken.FromObject(BitConverter.ToUInt64(data));
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			byte[] buf = BitConverter.GetBytes(data.ToObject<ulong>());
-			stream.Write(buf, 0, 8);
+			byte[] buf = BitConverter.GetBytes(ctx.Input.ToObject<ulong>());
+			ctx.Stream.Write(buf, 0, 8);
 		}
 	}
 
 	public class ProtoVarInt : ProtodefType
 	{
 		public new string Id = "varint";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			int numRead = 0;
 			int result = 0;
 			byte read;
 			do
 			{
-				read = (byte) stream.ReadByte();
+				read = (byte) ctx.Stream.ReadByte();
 				int value = (read & 0x7f);
 				result |= (value << (7 * numRead));
 
@@ -211,16 +211,16 @@ namespace ProtodefSharp.DefaultTypes
 			} while ((read & 0x80) != 0);
 			return JToken.FromObject(result);
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
-			uint value = data.ToObject<uint>();
+			uint value = ctx.Input.ToObject<uint>();
 			while ((value & -128) != 0)
 			{
-				stream.WriteByte((byte)((value & 0x7F) | 0x80));
+				ctx.Stream.WriteByte((byte)((value & 0x7F) | 0x80));
 				value >>= 7;
 			}
 
-			stream.WriteByte((byte)value);
+			ctx.Stream.WriteByte((byte)value);
 		}
 	}
 
@@ -229,11 +229,11 @@ namespace ProtodefSharp.DefaultTypes
 	public class ProtoCString : ProtodefType // TODO!!
 	{
 		public new string Id = "cstring";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
 			return JToken.FromObject("");
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
 			//
 		}
@@ -242,25 +242,27 @@ namespace ProtodefSharp.DefaultTypes
 	public class ProtoBuffer : ProtodefType // todo: add support for option bool 'rest'
 	{
 		public new string Id = "buffer";
-		public new JToken Read(Stream stream, JObject opts = null)
+		public new JToken Read(ProtodefContext ctx)
 		{
-			if (opts == null) throw new Exception("Options cannot be null for ProtodefType 'buffer'!");
-			if (opts.Properties().Any(p => p.Name == "countType")) {
-				string countType = opts.Properties().First(k => k.Name == "countType").Value.ToObject<string>();
+			if (ctx.Options == null) throw new Exception("Options cannot be null for ProtodefType 'buffer'!");
+			if (ctx.Options.Type != JTokenType.Object) throw new Exception("Options must be an object for ProtodefType 'buffer'!");
+			JObject opts = (JObject)ctx.Options;
+			if (opts.ContainsKey("countType")) {
+				string countType = opts.GetValue("countType").ToObject<string>();
 				// TODO!!
 			} else
 			{
-				if (!opts.Properties().Any(p => p.Name == "count")) throw new Exception("Either 'countType' or 'count' must be an option for ProtodefType 'buffer'");
-				int count = opts.Properties().First(k => k.Name == "count").Value.ToObject<int>();
+				if (!opts.ContainsKey("count")) throw new Exception("Either 'countType' or 'count' must be an option for ProtodefType 'buffer'");
+				int count = opts.GetValue("count").ToObject<int>();
 				byte[] buf = new byte[count];
 				for (int i = 0; i < count; i++)
 				{
-					buf += (byte)stream.ReadByte();
+					//buf += (byte)ctx.Stream.ReadByte();
 				}
 
 			}
 		}
-		public new void Write(Stream stream, JToken data, JObject opts = null)
+		public new void Write(ProtodefContext ctx)
 		{
 			//
 		}
--- ProtodefSharp/Protodef.cs
+++ ProtodefSharp/Protodef.cs
@@ -26,21 +26,50 @@ namespace ProtodefSharp
 			{ "varint", new ProtoVarInt() },
 		};
 
-		public Dictionary<string, ProtodefType> Types = Protodef.NativeTypes;
+		public Dictionary<string, ProtodefType> Types = new();
+		public ProtodefNamespace RootNamespace = new();
+		public Dictionary<string, ProtodefNamespace> Namespaces = new();
 
 		public Protodef()
 		{
 				
 		}
+
+		public void Parse(string data) => Parse(JToken.Parse(data));
+		public void Parse(JToken data)
+		{
+			if (data.Type != JTokenType.Object) throw new Exception("Root JToken is not an object!");
+			JObject obj = (JObject)data;
+
+		}
+	}
+
+	public class ProtodefNamespace
+	{
+		public ProtodefNamespace(string name = "root")
+		{
+
+		}
+	}
+
+	public class ProtodefContext
+	{
+		public Stream Stream = null;
+		public JObject Options = new JObject();
+		public Protodef Protodef;
+		public ProtodefNamespace Namespace;
+
+		// Only for Write()
+		public JToken Input = JToken.FromObject(null);
 	}
 
 	public class ProtodefType
 	{
 		public string Id;
-		public JToken Read(Stream stream, JObject opts = null) {
+		public JToken Read(ProtodefContext ctx) {
 			return JToken.FromObject(null);
 		}
-		public void Write(Stream stream, JToken data, JObject opts = null) { }
+		public void Write(ProtodefContext ctx) { }
 		// do we even need this
 		//public void SizeOf(Stream stream);
 	}
